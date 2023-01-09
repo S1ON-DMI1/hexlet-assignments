d10dc95c5dd8b148d77b33bb87248a85e4e1bdba
@hexlet/cli <support@hexlet.io>
2023-01-09 18:29:47 +0100

    submit java-collections-ru/lists

---
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -0,0 +1,12 @@
+package exercise;
+
+//import java.util.Arrays;
+//import java.util.ArrayList;
+
+// BEGIN
+class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Hexlet!");
+    }
+}
+//END
