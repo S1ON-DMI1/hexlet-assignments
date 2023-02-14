c00a7434ab8f282d03fdd01d6f449dc4ef788387
@hexlet/cli <support@hexlet.io>
2023-02-14 14:25:16 +0300

    submit java-collections-ru/maps

--- java-collections-ru/maps/src/main/java/exercise/App.java
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -4,5 +4,44 @@ import java.util.HashMap;
 import java.util.Map;
 
 // BEGIN
+    class App {
+    public static void main(String[] args) {
+        System.out.println(App.toString(App.getWordCount("the java is the best programming language java")));
+//        (App.toString(App.getWordCount("java is the best programming language java")));
+    }
+    public static Map<String, Integer> getWordCount(String sentence) {
+        String[] text = sentence.split(" ");
 
+        Map<String, Integer> textMap = new HashMap<>();
+
+        if (sentence.isEmpty()) {
+            return (textMap);
+        }
+        for (int i = 0; i < text.length; i++) {
+            textMap.put(text[i], 0);
+        }
+        for (int i = 0; i < text.length; i++) {
+            for (String s : textMap.keySet()) {
+                if (s.equals(text[i])) {
+                    textMap.put(s, textMap.get(s) + 1);
+                }
+            }
+        }
+        return textMap;
+    }
+
+        public static String toString(Map<String, Integer> words) {
+
+            if(words.isEmpty()) {
+                return ("{}");
+            }
+            StringBuilder text2 = new StringBuilder("{\n");
+
+            for(Map.Entry<String, Integer> word: words.entrySet()) {
+                text2.append("  " + word.getKey() + ": " + word.getValue() + "\n");
+            }
+            text2.append("}");
+            return text2.toString();
+        }
+    }
 //END
