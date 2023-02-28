25cacac0e7fcfc084f356840641d6417afd1ae5e
@hexlet/cli <support@hexlet.io>
2023-02-28 14:11:20 +0300

    submit java-collections-ru/tests

--- java-collections-ru/tests/src/test/java/exercise/AppTest.java
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -11,7 +11,11 @@ class AppTest {
     @Test
     void testTake() {
         // BEGIN
-        
+        List<Integer> wrong1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        System.out.println(App.take(wrong1, 2)); // => [1, 2]
+
+        List<Integer> wrong2 = new ArrayList<>(Arrays.asList(7, 3, 10));
+        System.out.println(App.take(wrong2, 8)); // => [7, 3, 10]
         // END
     }
 }
